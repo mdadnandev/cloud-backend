43a1cd61c23cf7023d513adde99084c6272344bc
mdadnandev <mdadnanazmi@gmail.com>
2026-09-04 16:45:32 +0530

    Fixed file open and download issue

--- src/main/java/com/project/hussainproject/drive/service/StorageService.java
+++ src/main/java/com/project/hussainproject/drive/service/StorageService.java
@@ -3,7 +3,6 @@ package com.project.hussainproject.drive.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -20,6 +19,7 @@ public class StorageService {
     @Value("${supabase.s3.bucket-name}")
     private String bucketName;
 
+    // 1. Upload ke liye URL (Pehle se tha)
     public String generateUploadUrl(String storageKey, String mimeType) {
         PutObjectRequest objectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
@@ -30,12 +30,23 @@ public class StorageService {
         PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(r -> r
                 .signatureDuration(Duration.ofMinutes(15))
                 .putObjectRequest(objectRequest)
-
         );
 
-        return  presignedRequest.url().toString();
-
+        return presignedRequest.url().toString();
     }
 
+    // 2. YEH NAYA METHOD HAI - File ko view/download karne ke liye URL
+    public String generateDownloadUrl(String storageKey) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(storageKey)
+                .build();
 
+        PresignedGetObjectRequest presignedRequest = presigner.presignGetObject(r -> r
+                .signatureDuration(Duration.ofHours(1)) // URL 1 ghante tak valid rahega
+                .getObjectRequest(getObjectRequest)
+        );
+
+        return presignedRequest.url().toString();
+    }
 }
